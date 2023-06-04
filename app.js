32b23c51a970046658177793da58f98776603408
Eason Jiang <EasonJiangabc@gmail.com>
2023-06-03 22:51:04 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const https = require("https");
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/",function(req,res){
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+    const url = "https://us8.api.mailchimp.com/3.0/lists/59f30178ce";
+
+    const data = {
+        members:[
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME:lastName,
+                }
+            }
+        ]
+    }
+
+    var jsonData = JSON.stringify(data);
+
+    var options = {
+        method:"POST",
+        auth:":a5854461e98068cf32e99e5cb1121576"
+    }
+
+    var request = https.request(url,options,function(response){
+        
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        } else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+        
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+
+  
+    })
+
+    request.write(jsonData);
+    request.end();
+})
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("server is running on 3000");
+})
